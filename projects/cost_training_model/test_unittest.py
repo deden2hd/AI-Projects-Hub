3a078ea88a52dd13cd001afed2f97c4db8ffe4b4
deden2hd <unknown271737@gmail.com>
2025-03-22 11:43:49 +0700

    Memperbaiki workflow dan test

--- projects/cost_training_model/test_unittest.py
+++ projects/cost_training_model/test_unittest.py
@@ -3,7 +3,7 @@ from main import cost_training_model
 
 class Test(unittest.TestCase):
     def test_normal_case(self):
-        self.assertEqual(cost_training_model(8, 2.50, 8, 7), (56, 140, 1120.0))
+        self.assertEqual(   cost_training_model(8, 2.50, 8, 7), (56, 140, 1120.0))
 
 if __name__ == '__main__':
     # cost_training_model(8, 2.50, 8, 7)
